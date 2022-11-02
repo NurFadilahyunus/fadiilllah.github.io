4f45d9f6396df1bada34bd017ba256d0c424897b
unknown <myitsdillah@gmail.com>
2022-11-01 21:20:59 -0700

    Fadillah

---
+++ index.js
@@ -0,0 +1,76 @@
+// ada 3 cara untuk memprint hasil di java script
+
+// 1.console.log();
+// 2.document.writeln();
+// 3.alert();
+
+// console.log("hello")
+
+// var a = 12;
+
+// console.log(a)
+
+// operator pada primatika ( * + - /)
+// var bil1 = 11;
+// var bil2 = 12;
+
+// var hasil = ( bil1 + bil2)
+// hasil1 = (bil1/bil2);
+// hasil2 = (bil1-bil2);
+// hasil3 = (bil1*bil2);
+
+// console.log(hasil);
+// console.log(hasil1);
+// console.log(hasil2);
+// console.log(hasil3);
+
+// operator logika (|| &&)
+// benar || benar = true
+// benar || salah = true
+// salah || benar = true
+// salah || salah = false 
+
+// benar && benar = ture 
+// benar && salah = false 
+// salah && benar = false 
+// salah && salah = false
+
+// let a =14 == "14";
+// console.log(a);
+
+// let nilai = 80;
+// if(nilai > 70){
+//     console.log("selamat kamu lulus")
+// } else {
+// console.log("kamu harus belajar lagi");
+// }
+
+// let nilai = 102;
+
+// if (nilai >=90 && nilai <=100){
+//     console.log(nilai +" nilai sangat memuaskan");
+// } else if(nilai >=80 && nilai <= 89){
+//     console.log(nilai + "nilai memuaskan");
+// } else if(nilai >= 70 && nilai <=79){
+//     console.log(nilai + "nilai cukup");
+// } else{
+//     console.log(nilai + "nilai kurang");
+
+// }
+
+
+//switch case
+
+const warna = "hitam";
+
+switch (warna) {
+    case "hitam":
+        console.log("warna hitam");
+        break;
+    case "putih":
+        console.log("warna putih");
+    break;
+    default:
+        console.log("tidak ada warnah pilihan");
+        break;
+}
---
+++ tugas.js
@@ -0,0 +1,37 @@
+const email ="myitsdillah@gmail.com";
+const pass  ="dilcos"
+
+if ( email === "myitsdillah@gmail.com"){
+    if (pass ==="dilcos"){
+        console.log("kamu berhasil login");
+    }else{
+        console.log("password salah");
+    }
+}else{
+        console.log("email belum terdaftar");
+    
+    }
+
+  //  karna 2 kecualinya 
+
+
+  
+
+  const salesmen ="dillah";
+const perusahaan  ="pt cahaya"
+
+if ( salesmen === "dilla"){
+    if (perusahaan ==="pt cahaya"){
+        console.log("kamu berhasil login");
+    }else{
+        console.log("password salah");
+    }
+}else{
+        console.log("email belum terdaftar");
+    
+    }
+
+  //  karna 2 kecualinya 
+
+
+  
